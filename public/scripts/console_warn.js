72333e86f3e7d18fdf88888617baac1ad3ed9f45
BaseProgrammer <74561974+BaseProgrammer@users.noreply.github.com>
2021-01-31 13:10:45 +0000

    Create console_warn.js

---
+++ public/scripts/console_warn.js
@@ -0,0 +1,4 @@
+console.log("%cYou have opened the console. \
+If you have accidentally open this, close this by clicking on the grey cross button on the top right corner. \
+Otherwise if you are here on purpose, do not paste anything here if someone told you to, as it is dangerous \
+and is an illegal act on our website.Close this immediately, thank you.", "font-size:24px; color:black;");
