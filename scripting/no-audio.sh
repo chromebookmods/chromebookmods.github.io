fdb196ba54c072ebd1fe4474143ffe35868f65c5
BaseProgrammer <74561974+BaseProgrammer@users.noreply.github.com>
2021-02-01 20:56:57 +0000

    Update no-audio.sh

--- scripting/no-audio.sh
+++ scripting/no-audio.sh
@@ -1,13 +1,7 @@
 sleep 2
-echo '[==>] CM: Installing "WGET"'
-sudo apt-get install wget
-echo '[==>] CM: Installing "alsa-utils"'
-sudo apt-get install alsa-utils
 echo '[==>] CM: Installing "asound.state" file from WGET'
-sleep 2
 echo '[==>] CM: Do note that this is not ChromebookMods official "asound.state" script. Please refer to the providing for the original source code.'
 echo '[==>] CM: https://github.com/krptg0/fix_braswell_audio'
-sleep 3
 mkdir tmp-folder/
 cd tmp-folder/
 wget "https://raw.githubusercontent.com/krptg0/fix_braswell_audio/master/asound.state"
