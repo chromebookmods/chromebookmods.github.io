f2ad3557cdca16603d22cc4fcb9e88f2a4969349
BaseProgrammer <74561974+BaseProgrammer@users.noreply.github.com>
2021-02-01 21:33:38 +0000

    Create zerobrane-lua-ide-downloader.sh

---
+++ scripting/zerobrane-lua-ide-downloader.sh
@@ -0,0 +1,22 @@
+set -e
+
+sleep 2
+
+wget "https://download.zerobrane.com/ZeroBraneStudioEduPack-1.90-linux.sh"
+clear
+
+bash ZeroBraneStudioEduPack-1.90-linux.sh
+
+clear
+
+echo "Would you like to launch the IDE? [y/n]"
+read _input
+
+if [[ $_input == "Y" || $_input == "y" ]]; then
+        echo "Starting"
+        echo "Use command 'zbstudio' to launch it again"
+        echo "Or use command 'zbstudio-uninstall' to uninstall it"
+else
+        echo "Use command 'zbstudio' to launch it"
+        echo "Or use command 'zbstudio-uninstall' to uninstall it"
+fi
